1e9b09991fd2da3f6ec132b52264e10aeb15e725
17862719591 <184974699@qq.com>
2023-10-20 17:45:30 +0800

    feat: 修复数据库方言BUG

--- src/main/java/org/tinycloud/paginate/dialect/AbstractDialect.java
+++ src/main/java/org/tinycloud/paginate/dialect/AbstractDialect.java
@@ -27,27 +27,4 @@ public abstract class AbstractDialect implements Dialect {
         sql.append(" ) TEMP_COUNT");
         return sql.toString();
     }
-
-    /**
-     * 分页查询适配
-     *
-     * @param oldSQL 原始sql
-     * @param page   分页参数对象
-     * @return 处理过后的sql
-     */
-    @Override
-    public String getPageSql(String oldSQL, Page<?> page) {
-        Integer pageNo = page.getPageNum();
-        Integer pageSize = page.getPageSize();
-        StringBuilder sql = new StringBuilder(oldSQL);
-
-        int offset = (pageNo - 1) * pageSize;
-        int limit = pageSize;
-        if (offset == 0) {
-            sql.append(" LIMIT ").append(limit);
-        } else {
-            sql.append(" LIMIT ").append(offset).append(",").append(limit);
-        }
-        return sql.toString();
-    }
 }
--- src/main/java/org/tinycloud/paginate/dialect/support/MySqlDialect.java
+++ src/main/java/org/tinycloud/paginate/dialect/support/MySqlDialect.java
@@ -1,6 +1,29 @@
 package org.tinycloud.paginate.dialect.support;
 
+import org.tinycloud.paginate.Page;
 import org.tinycloud.paginate.dialect.AbstractDialect;
 
-public class MySqlDialect extends AbstractDialect{
+public class MySqlDialect extends AbstractDialect {
+    /**
+     * 分页查询适配
+     *
+     * @param oldSQL 原始sql
+     * @param page   分页参数对象
+     * @return 处理过后的sql
+     */
+    @Override
+    public String getPageSql(String oldSQL, Page<?> page) {
+        Integer pageNo = page.getPageNum();
+        Integer pageSize = page.getPageSize();
+        StringBuilder sql = new StringBuilder(oldSQL);
+
+        int offset = (pageNo - 1) * pageSize;
+        int limit = pageSize;
+        if (offset == 0) {
+            sql.append(" LIMIT ").append(limit);
+        } else {
+            sql.append(" LIMIT ").append(offset).append(",").append(limit);
+        }
+        return sql.toString();
+    }
 }
